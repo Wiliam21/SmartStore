636883fe32d6ec4e65db3ab34338338e68ae7f14
Wiliam <apple.awhp@gmail.com>
2019-03-11 18:28:31 -0600

    ScannerBarcode

--- app/src/main/java/p8/example/puntoventa/VentaProductos.java
+++ app/src/main/java/p8/example/puntoventa/VentaProductos.java
@@ -1,7 +1,14 @@
 package p8.example.puntoventa;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class VentaProductos extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class VentaProductos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venta_productos);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result =IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result!=null){
+            Toast.makeText(this,result.getContents().toString(),Toast.LENGTH_LONG).show();
+        }
+        else {
+            super.onActivityResult(requestCode,resultCode,data);
+        }
+    }
+
+    public void Escanear(View view){
+        IntentIntegrator intent =new IntentIntegrator(this);
+        intent.setDesiredBarcodeFormats(IntentIntegrator.PRODUCT_CODE_TYPES);
+        intent.setCameraId(0);
+        intent.setBeepEnabled(true);
+        intent.initiateScan();
+        intent.setBarcodeImageEnabled(false);
     }
 }
