dbbafd70cb98d4c255e98c1762f0c17db1e52232
Wiliam <apple.awhp@gmail.com>
2019-03-15 20:51:56 -0600

    Imagenes BarCode

--- app/src/main/java/p8/example/puntoventa/Inventario.java
+++ app/src/main/java/p8/example/puntoventa/Inventario.java
@@ -1,7 +1,11 @@
 package p8.example.puntoventa;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Inventario extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class Inventario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventario);
+
     }
 }
