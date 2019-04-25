574ff45320673440c5fb22e60a8703dfeb7c7602
Wiliam <apple.awhp@gmail.com>
2019-04-24 22:40:38 -0500

    Editar Proveedor

---
+++ app/src/main/java/p8/example/puntoventa/EditarProveedorActivity.java
@@ -0,0 +1,13 @@
+package p8.example.puntoventa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarProveedorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_proveedor);
+    }
+}
