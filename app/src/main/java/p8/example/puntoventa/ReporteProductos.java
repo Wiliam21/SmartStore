bead4dcfeaf5636bb5eefd86efa2664c0961d0ac
Alumno_1 <diego01.zrd@gmail.com>
2019-05-03 10:08:54 -0500

    conexion entre activities de reporte

--- app/src/main/java/p8/example/puntoventa/ReporteProductos.java
+++ app/src/main/java/p8/example/puntoventa/ReporteProductos.java
@@ -1,13 +1,30 @@
 package p8.example.puntoventa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class ReporteProductos extends AppCompatActivity {
 
+    LinearLayout Lyt_Diario,Lyt_Semanal,Lyt_Mensual;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte_productos);
+        Lyt_Diario=(LinearLayout) findViewById(R.id.Lyt_Diario);
+        Lyt_Semanal=(LinearLayout) findViewById(R.id.Lyt_Semanal);
+        Lyt_Mensual=(LinearLayout) findViewById(R.id.Lyt_Mensual);
+
+    }
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
+            case R.id.Lyt_Diario: i = new Intent(this, ReporteDiario.class);startActivity(i);break;
+            case R.id.Lyt_Semanal: i = new Intent(this, ReporteSemanal.class);startActivity(i);break;
+            case R.id.Lyt_Mensual: i = new Intent(this, ReporteMensual.class);startActivity(i);break;
+            default:break;
+        }
     }
 }
--- app/src/main/java/p8/example/puntoventa/ReporteSemanal.java
+++ app/src/main/java/p8/example/puntoventa/ReporteSemanal.java
@@ -2,12 +2,17 @@ package p8.example.puntoventa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ReporteSemanal extends AppCompatActivity {
 
+    EditText txtFirstDate, txtLastDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte_semanal);
+        txtFirstDate=(EditText)findViewById(R.id.txtFirstDate);
+        txtLastDate=(EditText)findViewById(R.id.txtLastDate);
+
     }
 }
