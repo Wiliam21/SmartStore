b57cc7496d4a1ff6211eea8beb5f0e40117709a7
Alumno_1 <diego01.zrd@gmail.com>
2019-05-07 10:37:29 -0500

    Calendario v2

--- app/src/main/java/p8/example/puntoventa/ReporteDiario.java
+++ app/src/main/java/p8/example/puntoventa/ReporteDiario.java
@@ -1,8 +1,10 @@
 package p8.example.puntoventa;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,10 +12,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class ReporteDiario extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class ReporteDiario extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     Button btnSelectdate;
     TextView txtfecha;
@@ -24,30 +30,25 @@ public class ReporteDiario extends AppCompatActivity {
 
         btnSelectdate=(Button)findViewById(R.id.btnSelectdate);
         txtfecha=(TextView)findViewById(R.id.txtfecha);
-
-        /*cv.setOnDateChangeListener(new OnDateChangeListener() {@Override public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth)
-            { // TODO Auto-generated method stub initScheduleEvent(); } });
-                // new AlertDialog.Builder(MomAppActivity.this) .setTitle("Event Calendar") .setMessage("Click to schedule or view events.") .setView(ll) .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                // public void onClick(DialogInterface dialog, int whichButton) { //do nothing...yet } }).setNegativeButton("Cancel", new DialogInterface.OnClickListener()
-                // { public void onClick(DialogInterface dialog, int whichButton) { // Do nothing. } } ).show();
-
-    }*/
-    }
-    public void fecha(View view){
-        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        final LinearLayout Ll = (LinearLayout) inflater.inflate(R.layout.dailycalendar, null, false);
-        CalendarView Calendar = (CalendarView) Ll.getChildAt(0);
-        Calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        btnSelectdate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, final int year, final int month,final int day) {
-                new AlertDialog.Builder(ReporteDiario.this).setTitle("Elige la fecha").setMessage("Click en la fecha deseada").setView(Ll).setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        String date=day+"/"+month+"/"+year;
-                        txtfecha.setText(date);
-                    }
-                });
+            public void onClick(View view) {
+                DialogFragment datePicker= new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
             }
         });
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        Calendar c=Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+
+        txtfecha.setText(currentDateString);
     }
 }
+
